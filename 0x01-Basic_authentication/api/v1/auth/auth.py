ce47edbfad06fac880d7c0732e647db93385c0cc
felixtoo48 <felixtoo48@gmail.com>
2023-11-08 21:57:19 +0300

    updating require authentiction method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,16 @@ class Auth():
     """ class for managing API Authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ defining require authentication """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        for excluded_paths in excluded_paths:
+            # Check if path or path with a trailing slash is in excluded_path
+            if path == excluded_path or path == excluded_path + '/':
+                return False
+        # If the path is not in the list of excluded paths, return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header template """
