a13ac374bc9313271101515f25e2b5421bebf574
felixtoo48 <felixtoo48@gmail.com>
2023-11-08 21:12:50 +0300

    updating documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,15 +5,15 @@ from typing import List, TypeVar
 
 
 class Auth():
-    """ class for managing API Authentication"""
+    """ class for managing API Authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ defining require authentication"""
+        """ defining require authentication """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """ authorization header template """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """ current user authorization template """
         return None
