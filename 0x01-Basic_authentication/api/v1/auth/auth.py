22c3a25a669ea734259bec08927911ccc25a1127
felixtoo48 <felixtoo48@gmail.com>
2023-11-08 20:31:56 +0300

    adding required blank lines

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,14 +3,17 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
     """ class for managing API Authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ defining require authentication"""
         return false
+
     def authorization_header(self, request=None) -> str:
         """ """
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ """
         return None
