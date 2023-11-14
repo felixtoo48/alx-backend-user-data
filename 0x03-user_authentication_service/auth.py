4b335f6e0efb9da4c096700fe86f9fedffd3b259
felixtoo48 <felixtoo48@gmail.com>
2023-11-14 19:00:31 +0300

    adding password hashing method

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" auth file """
+
+import bcrypt
+
+
+def _hash_password(password):
+    """ takes in password string and
+    returns salted hash password in bytes
+    """
+    password_bytes = password.encode('utf-8')
+    hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
