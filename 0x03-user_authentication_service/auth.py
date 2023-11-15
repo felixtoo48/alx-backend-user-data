3f6eda2b1d97642d0565de9a5c9d1e96ecc90c98
felixtoo48 <felixtoo48@gmail.com>
2023-11-15 17:25:20 +0300

    removing .decode

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,7 +14,7 @@ def _hash_password(password: str) -> bytes:
     """
     password_bytes = password.encode('utf-8')
     hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-    return hashed_bytes.decode('utf-8')
+    return hashed_bytes
 
 
 class Auth:
