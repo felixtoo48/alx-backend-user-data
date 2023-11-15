37c3be6869f2aa7fe0eba232d9ac6db481c468bd
felixtoo48 <felixtoo48@gmail.com>
2023-11-15 18:22:51 +0300

    adding register user function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ Flask App """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,20 @@ app = Flask(__name__)
 def message() -> str:
     """ print message given """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """function to implement POST /users route """i
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": "email already registered"), 400
 
 
 if __name__ == "__main__":
