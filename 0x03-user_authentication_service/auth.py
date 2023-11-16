945abf95a51aca8876c5af643ddd8ee5e996fcd5
felixtoo48 <felixtoo48@gmail.com>
2023-11-16 17:37:47 +0300

    removing if email statement

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,10 +92,9 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """ generate and reset user's password token """
         try:
-            if email:
-                existing_user = self._db.find_user_by(email=email)
-                reset_token = _generate_uuid()
-                self._db.update_user(existing_user.id, reset_token=reset_token)
-                return reset_token
+            existing_user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(existing_user.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError()
