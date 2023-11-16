365b04480398d5273bfeb38e065c9631764fa19a
felixtoo48 <felixtoo48@gmail.com>
2023-11-16 16:36:54 +0300

    logout function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask App """
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth
 
 
@@ -42,6 +42,20 @@ def login():
         return res
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """ implementing logout function"""
+    # extract session id from cookie
+    session_id = request.cookies.get("session_id", None)
+    # find user by session id
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for(message))
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
