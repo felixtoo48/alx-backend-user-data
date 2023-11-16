5ad8493cdbd2894dc57cbe5c95697fe8330b3d5f
felixtoo48 <felixtoo48@gmail.com>
2023-11-16 17:35:53 +0300

    updating try except block

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,5 +97,5 @@ class Auth:
                 reset_token = _generate_uuid()
                 self._db.update_user(existing_user.id, reset_token=reset_token)
                 return reset_token
-        except Exception:
-            return ValueError()
+        except NoResultFound:
+            raise ValueError()
